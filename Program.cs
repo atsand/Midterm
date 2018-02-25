9ab79a9d4817d4113cf20ec09cd61c9de79db5d8
Aaron <atsand23@gmail.com>
2018-02-25 16:09:53 -0500

    Copying from partner repo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Controllers;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the General Store!\n");
+            AppNavigater.InitApp();
+        }
+    }
+}
+       
